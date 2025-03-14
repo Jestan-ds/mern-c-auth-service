ff3e0419095a4e83e48f8f5b3363ab983d9448ef
Jestan-ds <jestandsouza@gmail.com>
2025-03-14 11:14:00 +0530

    update .gitignore to include public directory and format JWKS conversion script

--- scripts/convertPemToJwks.js
+++ scripts/convertPemToJwks.js
@@ -4,5 +4,4 @@ import rsaPemToJwk from 'rsa-pem-to-jwk';
 const privateKey = fs.readFileSync('./certs/private.pem');
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unused-vars
-const jwks = rsaPemToJwk(privateKey,{use:'sig'},"public");
-
+const jwks = rsaPemToJwk(privateKey, { use: 'sig' }, 'public');
