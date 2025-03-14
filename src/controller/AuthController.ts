d402cdfc7529dd9263d86ea59415cf50c5e8d341
Jestan-ds <jestandsouza@gmail.com>
2025-03-14 13:11:18 +0530

    implement refresh token functionality and add validation middleware

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -177,4 +177,58 @@ export class AuthController {
     const user = await this.userService.findById(Number(req.auth.sub));
     res.json({ ...user, password: undefined });
   }
+
+  async refresh(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+      const payload: JwtPayload = {
+        sub: req.auth.sub,
+        role: req.auth.role,
+      };
+      const accessToken = this.tokenService.generateAccessToken(payload);
+      const user = await this.userService.findById(Number(req.auth.sub));
+      if (!user) {
+        const error = createHttpError(
+          400,
+          'User with token could not be found',
+        );
+        next(error);
+        return;
+      }
+
+      //persist refresh token
+      const newRefreshToken = await this.tokenService.persistRefreshToken(user);
+
+      //Delete old refresh token
+      await this.tokenService.deleteRefreshToken(Number(req.auth.id));
+
+      const refreshToken = this.tokenService.generateRefreshToken({
+        ...payload,
+        id: newRefreshToken.id,
+      });
+
+      res.cookie('accessToken', accessToken, {
+        domain: 'localhost',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60, //1hour
+        httpOnly: true,
+      });
+
+      res.cookie('refreshToken', refreshToken, {
+        domain: 'localhost',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60 * 24 * 365, //1week
+        httpOnly: true,
+      });
+
+      this.logger.info('User Has Been Logged In Successfully', {
+        id: user.id,
+      });
+
+      res.json({
+        id: user.id,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
